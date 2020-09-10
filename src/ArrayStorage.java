3b4e49c00fa50a1008dcea4fae9b58732afb4218
Andrei <livanovandrej99@gmail.com>
2020-09-10 19:26:26 +0700

    implement HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,61 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
+        Arrays.fill(storage, null);
+        size = 0;
     }
 
     void save(Resume r) {
+        if (getIndex(r.uuid) != -1) {
+            System.out.println("Resume " + r.uuid + " already exists!");
+        } else {
+            storage[size] = r;
+            size++;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " doesn't exists!");
+            return null;
+        }
+        return storage[index];
     }
 
     void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " doesn't exists!");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid == storage[i].uuid) return i;
+        }
+        return -1;
     }
 }
